7055857f7163c07cae11a9b2a66316bd66a7b169
Alamin <ahammedalamin600@gmail.com>
2023-09-16 15:05:14 +0600

    this is a api project

---
+++ script.js
@@ -0,0 +1,97 @@
+let url = 'https://dummyjson.com/posts';
+
+async function getData(params) {
+    try {
+        let response = await fetch(url);
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        let data = await response.json(); // Parse the response as JSON
+        makePost(data)
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+getData()
+
+function makePost(data) {
+    let array = data.posts;
+    array.forEach(element => {
+        callMeToCreatePost(element)
+    });
+}
+
+function callMeToCreatePost(element) {
+    let div = document.createElement('div');
+    div.innerHTML = `<div class="post">
+    <div class="postOwner">
+        <div class="highlightedComments flex">
+            <div class="profile"></div>
+            <div class="profileName">
+                <p class="name">Alamin Ahammed</p>
+                <p class="comment">Feeling good.🙂</p>
+            </div>
+        </div>
+    </div>
+    <div class="postCaption">
+    ${element.body}
+    </div>
+    <div class="postImage">
+        <img src="https://watermark.lovepik.com/photo/50085/3715.jpg_wh1200.jpg" alt="Image">
+    </div>
+    <div class="postReactions_and_postBtn_parent">
+        <div class="postReactions flex jc-sb">
+            <div class="totalReactions">&#10084;1k people liked this</div>
+            <div class="totalComments">💬1.2k comments</div>
+        </div>
+        <div class="postBtn flex">
+            <button class="reactionsBtn flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />
+                </svg>
+                <p>Like</p>
+            </button>
+            <button class="commentsBtn flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+                </svg>
+                <p>Comments</p>
+            </button>
+        </div>
+    </div>
+    <div class="highlightedComments flex">
+        <div class="profile"></div>
+        <div class="profileName">
+            <p class="name">Alamin Ahammed</p>
+            <p onclick="getAllComments(this)" class="comment">${element.title}</p>
+        </div>
+    </div>
+</div>`
+document.body.appendChild(div);
+}
+
+
+    async function getAllComments(params) {
+        let response = await fetch(' https://dummyjson.com/comments');
+        let data = await response.json();
+        
+        data.comments.forEach(element => {
+            let comment = document.createElement('div');
+            comment.innerHTML = `<div id="insertHere" class="highlightedComments flex">
+            <div class="profile"></div>
+            <div class="profileName">
+                <p class="name">${element.user.username}</p>
+                <p class="comment">${element.body}</p>
+            </div>
+        </div>`
+        
+        let highlightedComments = params.closest('.highlightedComments');
+        highlightedComments.insertAdjacentElement('afterend',comment);
+        })
+    }
